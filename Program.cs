1e16ae94b381f0dd3aa8c72a3d2748d4866b032c
Justin <Justin@DESKTOP-51D0A8D>
2023-10-12 21:45:08 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using dcAllahBot.config;
+using System.Threading.Tasks;
+using dcAllahBot.commands;
+
+namespace dcAllahBot
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; } 
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+            //5. Set up the Task Handler
+            Client.Ready += Client_Ready;
+           // Client.MessageCreated += MessageCreatedHandler;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { jsonReader.prefix },
+                EnableMentionPrefix = true,
+                EnableDms =true,
+                EnableDefaultHelp = false
+            };
+            //7. Register commands
+            Commands = Client.UseCommandsNext(commandsConfig);
+            Commands.RegisterCommands<basicCommands>();
+
+            //8. Connect to get the Bot online
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+       /* private static async Task MessageCreatedHandler(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs e)
+        {
+           // if(e.Channel.GetMessagesAsync().ToString == "allah")
+            await e.Channel.SendMessageAsync("OK?");
+        }*/
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
+
+
+
+
---
+++ commands/basicCommands.cs
@@ -0,0 +1,22 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Threading.Tasks;
+
+namespace dcAllahBot.commands
+{
+    public class basicCommands : BaseCommandModule
+    {
+        [Command("bomb")]
+        public async Task AllahWords(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync($"Allah Uakbar!!! {ctx.User.Username} wird in 5 Sekunden weggebombt");
+        }
+
+        [Command("allah")]
+        public async Task allah(CommandContext ctx)
+        {
+
+        }
+        
+    }
+}
